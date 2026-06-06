ec2157e2e82997ee48865b9583e5c3d795667adb
Harsha Penmatsa <penmatsaharsha@gmail.com>
2026-06-06 14:54:36 +0530

    Update app.py

--- app.py
+++ app.py
@@ -64,7 +64,7 @@ with st.sidebar:
 
     model = st.selectbox(
         "Choose Model",
-        ["llama3-8b-8192", "llama3-70b-8192", "mixtral-8x7b-32768", "gemma2-9b-it"],
+        ["llama-3.3-70b-versatile","llama3-8b-8192", "llama3-70b-8192", "mixtral-8x7b-32768", "gemma2-9b-it"],
         index=0
     )
 
